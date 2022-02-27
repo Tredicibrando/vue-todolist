0ea261487169c506e20a18075570c733988f489a
Tredicibrando <nicolosigiov@gmail.com>
2022-02-27 19:07:15 +0100

    step intermedio

--- js/app.js
+++ js/app.js
@@ -4,6 +4,28 @@ console.log('ciao');
 const app = new Vue ({
     el: '#app',
     data:{
-        message:'',
-    }
+        message: '',
+        items: [{
+            text: this.message,
+            data: false,
+        }],
+    },
+    methods:{
+        addNewItem(){
+            if(this.message === ''){
+                console.log('la stringa non contiene caratteri')
+            } else {
+                this.items.push(this.message);
+                this.message = '';
+                console.log(this.items,this.items.lenght)
+            }   
+        },
+
+        removeNewItem(i){
+            this.items.splice(i,1)
+        }
+  
+
+
+    },
 });
